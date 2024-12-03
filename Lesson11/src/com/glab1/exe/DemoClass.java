d0ff5e10036feaf044bdbe7996b007c6e5ca8aaf
Mathanki <mathanki091suren@gmail.com>
2024-12-03 17:00:55 -0500

    first commit

---
+++ Lesson11/src/com/glab1/exe/DemoClass.java
@@ -0,0 +1,10 @@
+package com.glab1.exe;
+
+public class DemoClass {
+    // create a generics method
+    public <T> void genericsMethod(T data) {
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+    }
+
+}
